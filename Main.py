094b34ef3880d30ddbc19e77df171f48924c1197
00urbina00 <edurbina2002@hotmail.com>
2023-04-27 23:40:17 -0600

    Nuevos cambios aplicados

--- Main.py
+++ Main.py
@@ -7,8 +7,6 @@ window = MainWindow()
 window.show()
 sys.exit(app.exec_())
 
-# Hola mundo!
-
 """
 create table conductores(id serial, codigo varchar[6], 
 						 nombre varchar[50], direcion varchar[100], numero char[12],
